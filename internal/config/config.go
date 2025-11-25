bacbf84651586df7ba2c379da46360f64d1a215d
Denis <denis.chernyshov@gloria-jeans.ru>
2025-11-25 16:29:28 +0300

    add config building

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,36 +1,67 @@
 package config
 
 import (
+	"flag"
+	"os"
 
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
-	Server ServerConfig
-	Log LogConfig
-	Storage StorageConfig
+	Server ServerConfig `yaml:"server"`
+	Log LogConfig `yaml:"log"`
+	Storage StorageConfig `yaml:"storage"`
 }
 
 type ServerConfig struct {
-	Host string
-	Port int
-	ReadTimeout int
-	
+	Host string `yaml:"host"`
+	Port int `yaml:"port"`	
 }
 
 type LogConfig struct {
-	Path string
-	Level int
+	Path string `yaml:"path"`
+	Level int `yaml:"log_level"`
 }
 
 type StorageConfig struct {
-	Host string
-	Port int
-	DBName string
-	Username string
-	Password string
-	TimeZone string
+	Host string `yaml:"host" env:"DB_HOST" env-default:"127.0.0.1"`
+	Port int `yaml:"port" env:"DB_PORT" env-default:"5432"`
+	DBName string `yaml:"db_name"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password" env:"PSTGRS_PASSWORD"`
+	TimeZone string `yaml:"timezone"`
 }
 
-func NewConfig() *Config {
-	return &Config{}
+func MustLoad() *Config {
+	path := loadPath()
+	if path == "" {
+		panic("Can`t read config file")
+	}
+
+	return loadConfig(path)
+}
+
+func loadPath() string {
+	var path string
+	flag.StringVar(&path, "config", "", "path to config file")
+	flag.Parse()
+	if path == "" {	
+		path = "./config/config.yaml"
+	}
+
+	return path
+}
+
+func loadConfig(path string) *Config {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
+	}
+	
+	return &cfg
 }
