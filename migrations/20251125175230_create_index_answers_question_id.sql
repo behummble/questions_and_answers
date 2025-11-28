b5c7d8f5eb45c5da501cd8acb17ec5bbcf0f9abe
Denis <denis.chernyshov@gloria-jeans.ru>
2025-11-28 10:54:41 +0300

    fix migration

--- migrations/20251125175230_create_index_answers_question_id.sql
+++ migrations/20251125175230_create_index_answers_question_id.sql
@@ -1,9 +1,11 @@
 -- +goose Up
+-- +goose NO TRANSACTION
 -- +goose StatementBegin
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_answers_question_id ON answers (question_id);
 -- +goose StatementEnd
 
 -- +goose Down
+-- +goose NO TRANSACTION
 -- +goose StatementBegin
 DROP INDEX CONCURRENTLY IF EXISTS idx_answers_question_id;
 -- +goose StatementEnd
